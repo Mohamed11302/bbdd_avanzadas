919157f1c84b4489330da00c0e71b7769dacfd81
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 <mohamed.essalhi@alu.uclm.es>
2024-02-18 15:59:06 +0100

    fix: comprobacion posible error

--- BBDD/diseno/gold.py
+++ BBDD/diseno/gold.py
@@ -2,6 +2,7 @@ import conexionBBDD.conexionBBDD as conBBDD
 import pandas as pd
 
 def diseno_gold(conn_str):
-    df_video = pd.read_csv('video.csv', index=False)
-    df_channel= pd.read_csv('channel.csv', index=False)
+    df_video = pd.read_csv('video.csv')
+    df_channel= pd.read_csv('channel.csv')
+    print("Leidos df_video y df_channel")
     
--- BBDD/main.py
+++ BBDD/main.py
@@ -6,6 +6,9 @@ import conexionBBDD.conexionBBDD as conBBDD
 
 if __name__ == "__main__":
     conn_str = conBBDD.get_bbdd_url()
+    test_conexion = conBBDD.verificar_conexion(conn_str)
+    if test_conexion != None:
+        print("Conexion establecida con el servidor")
     #bronze.create_bronze_schema(conn_str)
     #silver.preprocess_silver_scheme(conn_str)
     gold.diseno_gold(conn_str)
